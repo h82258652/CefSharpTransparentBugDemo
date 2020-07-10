2b5d2518cfc278548d20ed21ef0d615a13ea1050
h82258652 <842053625@qq.com>
2020-07-10 13:00:04 +0800

    commit

---
+++ Demo_Shared/MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using CefSharp;
+
+namespace Demo
+{
+    public partial class MainWindow
+    {
+        private void WebBrowser_IsBrowserInitializedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!WebBrowser.IsBrowserInitialized)
+            {
+                return;
+            }
+
+            const string html = "<html><head></head><body><h1>Hello World</h1></body></html>";
+            WebBrowser.LoadHtml(html);
+        }
+    }
+}
